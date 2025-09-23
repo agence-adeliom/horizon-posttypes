1527f3865d1f2809e63161ff8ee564254e98eb1f
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-09-23 12:06:12 +0200

    Add initial project structure with configuration files and ImportPostType command

---
+++ src/Console/Commands/ImportPostType.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adeliom\HorizonPostTypes\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ImportPostType extends Command
+{
+    public function handle(): void {}
+}
