d55c6c5f052901f8091a548c5a509bfa96da922a
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-09-23 16:51:45 +0200

    Add FAQ and LandingPage post types to available post types list

--- src/Services/HorizonPostTypesService.php
+++ src/Services/HorizonPostTypesService.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Adeliom\HorizonPostTypes\Services;
 
 use Adeliom\HorizonPostTypes\PostTypes\CustomerReview;
+use Adeliom\HorizonPostTypes\PostTypes\FAQ;
+use Adeliom\HorizonPostTypes\PostTypes\LandingPage;
 
 class HorizonPostTypesService
 {
     public static function getAvailablePostTypes(): array
     {
-        return [CustomerReview::class => []];
+        return [CustomerReview::class => [], FAQ::class => [], LandingPage::class => []];
     }
 }
